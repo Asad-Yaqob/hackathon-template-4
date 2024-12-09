e23d812cbd7e9b4e9fb3260b26a99d323e01f535
AsadYaqoob <asadmuhammad09123@gmail.com>
2024-12-09 14:18:18 +0500

    product feature fixed

--- src/app/components/common/home/ProductFeature.tsx
+++ src/app/components/common/home/ProductFeature.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-const ProductFeature = ({ text, color }) => {
+interface ProductFeatureProps {
+  text: string;
+  color: string; 
+}
+
+const ProductFeature = ({ text, color }: ProductFeatureProps ) => {
   return (
     <div className="flex items-center space-x-2 mb-2">
       <div className={`w-2 h-2 rounded-full ${color}`}></div>
