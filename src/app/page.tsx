d9ca9b0253680267132a1b1122fc7baba7563107
AsadYaqoob <asadmuhammad09123@gmail.com>
2025-01-16 23:20:15 +0500

    refactor: improve formatting and structure of Home component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,15 @@ import { ProductShowcase } from "../components/common/home/main/ProductShowcase"
 import TrendingProducts from "../components/common/home/main/TrendingProducts";
 import { DiscountItem } from "../components/common/home/main/DiscountItem";
 import TopCategories from "../components/common/home/main/TopCategories";
-import Image from "next/image";
+
 import { NewsLetter } from "@/components/common/home/main/NewsLetter";
 import { BrandsHeader } from "@/components/common/footer/BrandsHeader";
-import { FaPenNib } from "react-icons/fa";
-import { CalendarDays } from "lucide-react";
+
 import { homeBlogs } from "./data/sample_data";
 import { HomeBlogCard } from "@/components/common/blog/HomeBlogCard";
 export default function Home() {
   return (
-    < >
+    <>
       <HeroSection />
       <FeaturedProducts />
       <LatestProducts />
@@ -27,15 +26,23 @@ export default function Home() {
 
       <div className="text-center mt-[200px] mb-[100px]">
         <h1 className="font-josefin text-[42px] text-[#151875]">
-         Latest Blogs
+          Latest Blogs
         </h1>
       </div>
 
       <div className="flex items-center justify-center mb-[100px]">
-    {homeBlogs.map((blog, index) => <HomeBlogCard isActive={blog.isActive} name={blog.name} date={blog.date} description={blog.description} image={blog.image} writerName={blog.writeName} key={index} />)}
-  </div>
-
-
+        {homeBlogs.map((blog, index) => (
+          <HomeBlogCard
+            isActive={blog.isActive}
+            name={blog.name}
+            date={blog.date}
+            description={blog.description}
+            image={blog.image}
+            writerName={blog.writeName}
+            key={index}
+          />
+        ))}
+      </div>
     </>
   );
 }
